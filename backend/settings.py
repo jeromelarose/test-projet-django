ae04d299b5c1fbd373e7eb0e4f02f4ec96f75d8a
jeromelarose <jerome.larose35@gmail.com>
2024-09-26 03:57:31 +0200

    config database renderr

--- backend/settings.py
+++ backend/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ou un autre chemin selon ta structure
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
